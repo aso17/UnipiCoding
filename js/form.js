f5c7c9ce1caf901e0b1cca48c2ef11851a1a370b
aso17 <bagussajiwo305@gmail.com>
2021-10-09 23:25:21 +0700

    Update 9
    Menambahkan fitur form Kirim pesan

--- js/form.js
+++ js/form.js
@@ -1,9 +1,24 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbw0Jmf5N54HPdMne3X0o-ndAaDs-3O8USntcumHDPQcgFSvyBY1HEeuaxzx9v0CHUAv/exec";
 const form = document.forms["Pesan-Unipi"];
+const btnKirim = document.querySelector("#kirim");
+const btnLoading = document.querySelector("#loading");
+const alert = document.querySelector("#alert");
+// console.log(alert);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  //menambah dan
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) => {
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      //muncul alert
+      alert.classList.toggle("d-none");
+      //reset form
+      form.reset();
+      console.log("Success!", response);
+    })
     .catch((error) => console.error("Error!", error.message));
 });
